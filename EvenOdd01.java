ca9fca777e068545960c382c7181831d44a7006e
kmw9145 <162389778+kmw9145@users.noreply.github.com>
2024-03-19 17:16:42 +0900

    Add files via upload

---
+++ EvenOdd01.java
@@ -0,0 +1,34 @@
+package chap07;
+
+import java.util.Scanner;
+
+public class EvenOdd01 {
+
+	public static void decideEvenOdd() {
+			System.out.println("숫자를 입력하세요.");
+			Scanner input = new Scanner(System.in);
+			int num = input.nextInt();
+			
+			
+			if (num % 2 == 0) {
+				System.out.println(">> 짝수입니다.");
+			}
+			else {
+				System.out.println(">> 홀수입니다.");
+			}
+	}
+		public static void main(String[] args) {
+			
+			int bContinue = 1;
+			
+			while (bContinue != 0) {
+				decideEvenOdd();
+				
+			System.out.println("계속 진행 하시겠습니까? (0-멈춤/1-계속1) : " );
+			Scanner input = new Scanner(System.in);
+			bContinue = input.nextInt();
+		}
+		System.out.println("다음에 또 봐요 ㅎ");
+	}
+
+}
